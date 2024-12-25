9aefce690e68cdf7e612e73e5a9b92bb6e12aef9
Anjalii <118976916+Anjalithapa98@users.noreply.github.com>
2024-12-25 10:38:39 -0800

    Update MobileNav.js

--- Src/Component/MobileNav.js
+++ Src/Component/MobileNav.js
@@ -18,43 +18,31 @@ const menuVariants = {
 };
 const MobileNav = () => {
     const [openMenu, setOpenMenu] = useSate(false);
-    return ( <
-        nav className = 'text-primary xl:hidden' >
+    return ( <nav className = 'text-primary xl:hidden' >
 
-        <
-        div onClick = {
+        <div onClick = {
             () => setOpenMenu(true) }
         className = 'text-3xl cursor-pointer' >
-        <
-        CgMenuRight / >
+        <CgMenuRight />
 
-        <
-        /div> <
+        </div> <
         motion.div variants = { menuVariants }
         initial = 'hidden'
         animate = { openMenu ? 'show' : '' }
         className = 'bg-white shadow-2xl w-full absolute top-0 right-0 max-w-xs h-screen z-20' >
 
-        <
-        div onClick = {
+        <div onClick = {
             () => setOpenMenu(false) }
         className = 'text-4xl absolute z-30 left-4 top-14 text-primary cursor-pointer' >
-        <
-        IoMdClose > < /IoMdClose> <
-        /div> <
-        /motion.div>
+        < IoMdClose > </IoMdClose> </div> </motion.div>
 
-        <
-        ul className = 'h-full flex flex-col justify-center items-center gap-y-8 text-primary font-primary font-bold text-3xl' >
-        <
-        li >
-        <
-        Link to = '/' > Home < /Link> <
-        /li> <
-        /ul>
+        <ul className = 'h-full flex flex-col justify-center items-center
+            gap-y-8 text-primary font-primary font-bold text-3xl' >
+        <li >
+        <Link to = '/' > Home </Link> </li>
+            </ul>
 
-        <
-        /nav>
+        </nav>
     );
 };
 
