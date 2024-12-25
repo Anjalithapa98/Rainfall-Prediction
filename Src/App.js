80d0f6ed510dad444ad2a38b77666de25168eb98
Anjalii <118976916+Anjalithapa98@users.noreply.github.com>
2024-12-26 00:12:29 +0545

    Add files via upload

---
+++ Src/App.js
@@ -0,0 +1,33 @@
+import React, { useContext } from 'react';
+import Header from './Components/Header';
+import AnimRoutes from './Components/AnimRoutes';
+
+import { BrowserRouter as Router } from 'react-router-dom';
+import { motion } from 'framer-motion';
+import CursorContext from './context/CursorContext';
+
+const App = () => {
+    const { cursorVariants, cursorBG } = useContext(CursorContext)
+    return ( <
+        >
+        <
+        Router >
+        <
+        Header >
+        <
+        /Header> <AnimRoutes >
+
+        <
+        /AnimRoutes> </Router >
+
+        <
+        motion.div variants = { cursorVariants }
+        animate = { cursorBG }
+        className = 'w-[32px] h-[32px] bg-primary fixed top-0 left-0 pointer-events-none z-50 rounded-full' >
+        <
+        /motion.div> <
+        />
+    );
+};
+
+export default App;
