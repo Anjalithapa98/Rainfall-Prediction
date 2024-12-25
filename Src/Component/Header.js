604418b01caf429c31b34810abf95133570c4a8a
Anjalii <118976916+Anjalithapa98@users.noreply.github.com>
2024-12-25 10:36:00 -0800

    Update Header.js

--- Src/Component/Header.js
+++ Src/Component/Header.js
@@ -9,34 +9,27 @@ const Header = () => {
     const { mouseEnterHandler, mouseLeaveHandler } = useContext(CursorContext)
 
     return <header className = 'bg-pink-200 ficed w-full px-[30px] lg:px-[100px] z-30 h-[100px] 
-    lg: h - [140 px] flex items - center ' > Header < /header>; <
-        div className = 'flex flex-col lg:flex-row lg:items-center w-full justify-between' >
+    lg: h-[140 px] flex items-enter ' > Header < /header>;
+        <div className = 'flex flex-col lg:flex-row lg:items-center w-full justify-between' >
 
-        <
-        Link
-
-    onMouseEnter = { mouseEnterHandler }
+        <Link
+onMouseEnter = { mouseEnterHandler }
     onMouseLeave = { mouseLeaveHandler }
     to = { '/' }
     className = 'max-w-[200px]' >
-        <
-        img src = { rainfalllogo }
-    alt = '' / >
+        <img src = { rainfalllogo }
+    alt = '' />
 
-        <
-        /Link> <
-    nav className = 'hidden xl:flex gap-x-12 font-semibold' > nav <
-        Link
+        </Link> <
+    nav className = 'hidden xl:flex gap-x-12 font-semibold' > nav
+        <Link
     onMouseEnter = { mouseEnterHandler }
     onMouseLeave = { mouseLeaveHandler }
     to = { '/' }
     className = 'text-[#696c6d] hover:text-primary transition' > Home < /Link>
 
-    <
-    /nav> <
-    MobileNav / >
-        <
-        /div>
+    </nav> < MobileNav />
+        </div>
 };
 
 export default Header;
