aef52ac2a78125174fec3294cef4f3c7c9693d81
Anjalii <118976916+Anjalithapa98@users.noreply.github.com>
2024-12-25 10:31:49 -0800

    Update App.js

--- Src/App.js
+++ Src/App.js
@@ -8,25 +8,17 @@ import CursorContext from './context/CursorContext';
 
 const App = () => {
     const { cursorVariants, cursorBG } = useContext(CursorContext)
-    return ( <
-        >
-        <
-        Router >
-        <
-        Header >
-        <
-        /Header> <AnimRoutes >
+    return ( <>
+        <Router >
+        <Header >
+        </Header> <AnimRoutes >
 
-        <
-        /AnimRoutes> </Router >
+        </AnimRoutes> </Router >
 
-        <
-        motion.div variants = { cursorVariants }
+        <motion.div variants = { cursorVariants }
         animate = { cursorBG }
         className = 'w-[32px] h-[32px] bg-primary fixed top-0 left-0 pointer-events-none z-50 rounded-full' >
-        <
-        /motion.div> <
-        />
+        </motion.div> </>
     );
 };
 
